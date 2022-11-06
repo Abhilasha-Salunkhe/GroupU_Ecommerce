3d6e9348fbb5e99939d47f3e0de48376b97a8a17
Komals82374 <117304551+Komals82374@users.noreply.github.com>
2022-11-06 18:16:03 +0530

    Add files via upload

---
+++ userlogin.sql
@@ -0,0 +1,11 @@
+use ecommercedb;
+
+create table userLogin (
+   first_Name varchar(255),
+   last_Name varchar (255),
+   user_Name varchar(255),
+   user_Password VARCHAR(255)
+   );
+   
+SELECT * FROM userLogin;
+describe userlogin;
